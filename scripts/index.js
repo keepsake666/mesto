2c6ff01b951da53132367eaf5b80f2c545c708a1
Sergey Veselov <veselovsa@list.ru>
2022-01-15 14:49:09 +0900

    feat: added JS redact Info block

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,36 @@ const profileOpenPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.form__close');
 
-function openPopup() {
+function openPopup(evt) {
+    evt.preventDefault()
     popup.classList.add('popup_opened')
 }
 
-function closePopup() {
+function closePopup(evt) {
+    evt.preventDefault()
     popup.classList.remove('popup_opened')
 }
 
 profileOpenPopupButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
-popup.addEventListener('click', function (event) {
-    if (event.target === event.currentTarget) {
-        closePopup()
-    }
-}) 
-  
+// Открытие и закрытие попап
+
+let nameInput = document.querySelector('.name-input');
+let jobInput =  document.querySelector('.job-input');
+let saveFormButton = document.querySelector('.form__buttod');
+let profileInfo = document.querySelector('.profile__info');
+
+let profileName = document.querySelector('.profile__name');
+let profileText = document.querySelector('.profile__text');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileText.textContent = jobInput.value;
+    popup.classList.remove('popup_opened')
+}
+
+saveFormButton.addEventListener('click', formSubmitHandler);
+
+// добавление данных из форм 
